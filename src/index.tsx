8e3a69da7e23050e0115d9ed35c45e0766232bd8
Rafal Porzucek <r.porzucek@gmail.com>
2022-02-18 10:58:13 +0000

    Execute code with srcdoc

--- src/index.tsx
+++ src/index.tsx
@@ -38,6 +38,12 @@ const App = () => {
     setCode(result.outputFiles[0].text);
   };
 
+  const html = `
+    <script>
+      ${code}
+    </script>
+  `;
+
   return (
     <div>
       <textarea
@@ -48,6 +54,7 @@ const App = () => {
         <button onClick={onClick}>Submit</button>
       </div>
       <pre>{code}</pre>
+      <iframe title="code-iframe" sandbox="allow-scripts" srcDoc={html} />
     </div>
   );
 };
