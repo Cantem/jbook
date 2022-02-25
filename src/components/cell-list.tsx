286e4a53c77dca1b962353fc172bdd736d00acf2
Rafal Porzucek <r.porzucek@gmail.com>
2022-02-25 11:32:06 +0000

    Create cell item and list

---
+++ src/components/cell-list.tsx
@@ -0,0 +1,18 @@
+import { useTypedSelector } from "../hooks/use-typed-selector";
+import { Cell } from "../state";
+import CellListItem from "./cell-list-item";
+
+const CellList: React.FC = () => {
+  // @ts-ignore
+  const cells = useTypedSelector(({ cells: { order, data } }) =>
+    order.map((id: string) => data[id])
+  );
+
+  const renderedCells = cells.map((cell: Cell) => (
+    <CellListItem key={cell.id} cell={cell} />
+  ));
+
+  return <div>{renderedCells}</div>;
+};
+
+export default CellList;
